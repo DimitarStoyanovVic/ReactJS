1d769bd3eadcf75fe4129e7823d176e3a15d0785
d.stoyanov <d.stoyanov.vic@gmail.com>
2022-04-09 01:37:10 +0300

    27. Monsters Rolodex - Component State

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -4,25 +4,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Dimitar'
+    }
+  }
+
   render() {
     return (
-        <div className="App">
-          <h1>Hi</h1>
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hi {this.state.name}</p>
+          <button>Change Name</button>
+        </header>
+      </div>
     );
   }
 }
