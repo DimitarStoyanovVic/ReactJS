14cc0f3af03848bd9023e4ba3253a82ab07d554a
d.stoyanov <d.stoyanov.vic@gmail.com>
2022-04-09 03:34:11 +0300

    33. Monsters Rolodex - Keys for Mapping

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -11,15 +11,19 @@ class App extends Component {
       monsters: [
         {
           name: 'Linda',
+          id: 1,
         },
         {
           name: 'Frank',
+          id: 2,
         },
         {
           name: 'Jacky',
+          id: 3,
         },
         {
           name: 'Andrei',
+          id: 4,
         }
       ],
     };
@@ -30,7 +34,11 @@ class App extends Component {
       <div className="App">
         {
           this.state.monsters.map((monster) => {
-            return <h1>{monster.name}</h1>;
+            return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
+            );
           })
         }
       </div>
