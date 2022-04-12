25754f38c4199a05e23450a4eefa86d9874ed0a6
d.stoyanov <d.stoyanov.vic@gmail.com>
2022-04-12 12:12:09 +0300

    37. Monsters Rolodex - Renders & Re-renders in React

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -10,9 +10,11 @@ class App extends Component {
     this.state = {
       monsters: []
     };
+    console.log('1');
   }
 
   componentDidMount() {
+    console.log('3');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users =>
@@ -27,6 +29,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('2');
     return (
       <div className="App">
         {
