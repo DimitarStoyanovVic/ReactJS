93c745a729e758664a5e40093cb9fc2f8f80d82b
d.stoyanov <d.stoyanov.vic@gmail.com>
2022-04-13 07:16:18 +0300

    42. Monsters Rolodex - Optimizations

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -26,9 +26,21 @@ class App extends Component {
       );
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    })
+  }
+
   render() {
-    const filteredMonsters = this.state.monsters.filter(monster => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter(monster => {
+      // console.log('yes');
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
@@ -36,14 +48,7 @@ class App extends Component {
         <input className='search-box'
                type='search'
                placeholder='search monsters'
-               onChange={(event) => {
-                 const searchField = event.target.value.toLocaleLowerCase();
-
-                 this.setState(() => {
-                   return { searchField };
-                 });
-                }
-               }
+               onChange={onSearchChange}
         />
 
         {filteredMonsters.map((monster) => {
