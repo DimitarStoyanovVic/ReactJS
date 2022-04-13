37518772104586b62460fc49978d60c4aba61315
d.stoyanov <d.stoyanov.vic@gmail.com>
2022-04-13 05:57:24 +0300

    39. Monsters Rolodex - Searching & Filtering

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -10,11 +10,11 @@ class App extends Component {
     this.state = {
       monsters: []
     };
-    console.log('1');
+    // console.log('constructor');
   }
 
   componentDidMount() {
-    console.log('3');
+    // console.log('disMount');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users =>
@@ -29,9 +29,23 @@ class App extends Component {
   }
 
   render() {
-    console.log('2');
+    // console.log('render');
     return (
       <div className="App">
+        <input className='search-box'
+               type='search'
+               placeholder='search monsters'
+               onChange={(event) => {
+                 const searchString = event.target.value.toLocaleLowerCase();
+                 const filteredMonsters = this.state.monsters.filter(monster => {
+                   return monster.name.toLocaleLowerCase().includes(searchString);
+                 });
+
+                 this.setState(() => {
+                   return { monsters: filteredMonsters };
+                 });
+                }
+               } />
         {
           this.state.monsters.map((monster) => {
             return (
