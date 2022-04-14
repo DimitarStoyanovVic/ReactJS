49b3be7506062675296c9220be6705098908271d
d.stoyanov <d.stoyanov.vic@gmail.com>
2022-04-14 12:19:17 +0300

    48. Monsters Rolodex - SearchBox Component

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 
 import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 
 import './App.css';
 
@@ -45,10 +46,10 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input className='search-box'
-               type='search'
-               placeholder='search monsters'
-               onChange={onSearchChange}
+        <SearchBox
+          className='search-box'
+          placeholder='search monsters'
+          onChangeHandler={onSearchChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
