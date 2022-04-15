ac161dc4745dc9b95383a7adcfc5a67c28303607
d.stoyanov <d.stoyanov.vic@gmail.com>
2022-04-15 12:51:59 +0300

    58. Monsters Rolodex - Infinite Re-rendering

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,4 +1,4 @@
-import {Component, useState} from 'react';
+import { useState, useEffect } from 'react';
 
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
@@ -6,14 +6,23 @@ import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
 const App = () => {
-  console.log('render');
   const [searchField, setSearchField] = useState('a');
-  console.log(searchField);
+  const [monsters, setMonsters] = useState([]);
+
+  console.log('render');
+
+  fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => setMonsters(users) );
 
   const onSearchChange = (event) => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
   }
+
+  const filteredMonsters = monsters.filter(monster => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
 
   return (
     <div className="App">
@@ -24,7 +33,8 @@ const App = () => {
         placeholder='search monsters'
         onChangeHandler={onSearchChange}
       />
-      {/*<CardList monsters={filteredMonsters} />*/}
+
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 }
