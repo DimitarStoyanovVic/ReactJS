a99a9c046446661e01fa96335ef172ff78e27d00
d.stoyanov <d.stoyanov.vic@gmail.com>
2022-04-19 11:17:47 +0300

    67. DOM Paint Flashing

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 
 const App = () => {
   const [searchField, setSearchField] = useState('');
+  const [title, setTitle] = useState('');
   const [monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilterMonsters] = useState(monsters);
 
@@ -31,14 +32,26 @@ const App = () => {
     setSearchField(searchFieldString);
   }
 
+  const onTitleChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setTitle(searchFieldString);
+  }
+
   return (
     <div className="App">
-      <h1 className='app-title'>Monsters Rolodex</h1>
+      <h1 className='app-title'>{title}</h1>
 
       <SearchBox
         className='monsters-search-box'
         placeholder='search monsters'
         onChangeHandler={onSearchChange}
+      />
+
+      <br />
+      <SearchBox
+        className='title-search-box'
+        placeholder='search title'
+        onChangeHandler={onTitleChange}
       />
 
       <CardList monsters={filteredMonsters} />
