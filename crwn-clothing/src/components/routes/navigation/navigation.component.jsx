7d2abfcb8c85aa320b07d332e8704aab7883b4b9
d.stoyanov <d.stoyanov.vic@gmail.com>
2022-05-03 15:57:17 +0300

    86. Styling for Navigation + Logo

--- crwn-clothing/src/components/routes/navigation/navigation.component.jsx
+++ crwn-clothing/src/components/routes/navigation/navigation.component.jsx
@@ -1,6 +1,10 @@
 import { Fragment } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
+import { ReactComponent as  CrwnLogo } from "../../../assets/crown.svg";
+
+import './navigation.styles.scss';
+
 const Navigation = () => {
   return (
     <Fragment>
