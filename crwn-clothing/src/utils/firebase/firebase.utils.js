0e91724acd8f2ac83904d81788f842141c022957
d.stoyanov <d.stoyanov.vic@gmail.com>
2022-05-03 19:20:55 +0300

    91. Authenticating With Firebase - correction

--- crwn-clothing/src/utils/firebase/firebase.utils.js
+++ crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -13,13 +13,13 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBhK0mIOF5JderIRyW00IJf2aObmJxTlMA",
-  authDomain: "crwn-clothing-db-d4cf7.firebaseapp.com",
-  projectId: "crwn-clothing-db-d4cf7",
-  storageBucket: "crwn-clothing-db-d4cf7.appspot.com",
-  messagingSenderId: "469698263569",
-  appId: "1:469698263569:web:3dd4aaaf9c369da47a0fb0",
-  measurementId: "G-GGZFKD9P7M"
+  apiKey: "AIzaSyA5_fNFUusacc9k77c9mte-KsFxb9jUEb8",
+  authDomain: "crwn-clothing-db-479ee.firebaseapp.com",
+  projectId: "crwn-clothing-db-479ee",
+  storageBucket: "crwn-clothing-db-479ee.appspot.com",
+  messagingSenderId: "48821785992",
+  appId: "1:48821785992:web:9f394ee9de65889573163d",
+  measurementId: "G-Q9KH2499E8"
 };
 
 // Initialize Firebase
