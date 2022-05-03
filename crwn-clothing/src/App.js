9a38e99d4171991e7595f2a398b57019fe100c2e
d.stoyanov <d.stoyanov.vic@gmail.com>
2022-05-03 15:30:14 +0300

    85. React Router Link

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,21 +1,10 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
+import Navigation from "./components/routes/navigation/navigation.component";
 import Home from './components/routes/home/home.component';
 
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  )
-}
-
 const Shop = () => {
-  return (
-    <h1>I am the shop page</h1>
-  )
+  return <h1>I am the shop page</h1>;
 }
 
 const App = () => {
@@ -23,7 +12,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />}/>
-        <Route path='shop' element={<Home />}/>
+        <Route path='shop' element={<Shop />}/>
       </Route>
     </Routes>
   );
