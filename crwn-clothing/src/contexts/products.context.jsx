b4481f7559830b9bb72dedf056ec6734de34235b
d.stoyanov <d.stoyanov.vic@gmail.com>
2022-05-25 14:09:24 +0300

    127. addCollectionAndDocuments Pt.2
    Signed-off-by: d.stoyanov <d.stoyanov.vic@gmail.com>

--- crwn-clothing/src/contexts/products.context.jsx
+++ crwn-clothing/src/contexts/products.context.jsx
@@ -1,13 +1,12 @@
-import { createContext, useState } from 'react';
-
-import SHOP_DATA from '../shop-data.js';
+import {createContext, useEffect, useState} from 'react';
 
 export const ProductsContext = createContext({
   products: []
 });
 
-export const ProductsProvider = ({children}) => {
+export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
+
   const value = { products };
   return (
     <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
