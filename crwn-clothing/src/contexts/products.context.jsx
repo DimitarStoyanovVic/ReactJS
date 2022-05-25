e04044d9564fd6f1511951bb636eedf0542796fd
d.stoyanov <d.stoyanov.vic@gmail.com>
2022-05-25 14:33:17 +0300

    128. Get Products + Categories From Firestore
    Signed-off-by: d.stoyanov <d.stoyanov.vic@gmail.com>

--- crwn-clothing/src/contexts/products.context.jsx
+++ crwn-clothing/src/contexts/products.context.jsx
@@ -1,11 +1,22 @@
 import {createContext, useEffect, useState} from 'react';
 
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
+
 export const ProductsContext = createContext({
   products: []
 });
 
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+    }
+
+    getCategoriesMap();
+  }, []);
 
   const value = { products };
   return (
