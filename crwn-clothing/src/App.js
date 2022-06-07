db7680d6a7c6143d13a04073f1dc28156ed01a98
d.stoyanov <d.stoyanov.vic@gmail.com>
2022-06-07 14:37:51 +0300

    176. Redux-Saga: Check User Session Saga Pt. 2
    Signed-off-by: d.stoyanov <d.stoyanov.vic@gmail.com>

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -10,13 +10,13 @@ import Home from './routes/home/home.component';
 import Authentication from "./routes/authentication/authentication.component";
 import Shop from './routes/shop/shop.component';
 import Checkout from "./routes/checkout/checkout.component";
-import { setCurrentUser } from "./store/user/user.action";
+import { checkUserSession } from "./store/user/user.action";
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    getCurrentUser().then((user) => console.log(user));
+    dispatch(checkUserSession());
   }, []);
 
   return (
