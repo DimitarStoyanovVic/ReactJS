6f1166d5485e5eda734ba729545b34fe65208249
d.stoyanov <d.stoyanov.vic@gmail.com>
2022-06-07 12:37:12 +0300

    171. Asynchronous Redux: Redux-Saga
    Signed-off-by: d.stoyanov <d.stoyanov.vic@gmail.com>

--- crwn-clothing/src/store/store.js
+++ crwn-clothing/src/store/store.js
@@ -2,9 +2,10 @@ import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from 'redux-persist';
 import logger from "redux-logger";
 import storage from 'redux-persist/lib/storage';
-import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 
 import { rootReducer } from "./root-reducer";
+import {rootSaga} from "./root-saga";
 
 const persistConfig = {
   key: 'root',
@@ -12,9 +13,14 @@ const persistConfig = {
   whitelist: ['cart'],
 }
 
+const sagaMiddleware = createSagaMiddleware();
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger, thunk].filter(Boolean);
+const middleWares = [
+  process.env.NODE_ENV === 'development' && logger,
+  sagaMiddleware
+].filter(Boolean);
 
 const composeEnhancer =
   (process.env.NODE_ENV !== 'production' &&
@@ -25,5 +31,7 @@ const composeEnhancer =
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
